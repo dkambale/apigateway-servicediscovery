8488675f8f6cf7618eb5768401b12458992f50b0
Dadasaheb <c-dadasaheb.kambale@edifecs.com>
2022-09-16 15:50:41 +0530

    initial commit

---
+++ service1/src/main/java/com/codingworld/service1/controller/Service1Controller.java
@@ -0,0 +1,18 @@
+package com.codingworld.service1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Service1Controller {
+
+  @GetMapping(name = "/say",value = "/say")
+  public String sayHello() {
+
+    return "Hello From Service1";
+  }
+
+
+}
